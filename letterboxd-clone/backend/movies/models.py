ea5e86de5043aea873645ae29a092bb06b3c1b6a
Jaswant M K <jaswantkkumar1801@gmail.com>
2026-04-01 02:00:07 +0530

    fix: allow null backdrop and poster paths to prevent data load crash

--- letterboxd-clone/backend/movies/models.py
+++ letterboxd-clone/backend/movies/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class MovieCache(models.Model):
     tmdb_id = models.IntegerField(unique=True, db_index=True)
     title = models.CharField(max_length=255)
-    poster_path = models.CharField(max_length=255, blank=True)
-    backdrop_path = models.CharField(max_length=255, blank=True)
+    poster_path = models.CharField(max_length=255, blank=True, null=True)
+    backdrop_path = models.CharField(max_length=255, blank=True, null=True)
     overview = models.TextField(blank=True)
     release_date = models.DateField(null=True, blank=True)
     genres = models.JSONField(default=list)
